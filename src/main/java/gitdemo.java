a063c2b51a5d086bcc07bb9e99c4d346c69a9985
luoluoluo <2091823062@qq.com>
2022-12-09 17:32:40 +0800

    1

---
+++ src/main/java/gitdemo.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName gitdemo
+ * @Description TODO
+ * @Author @luo
+ * @Date 2022/12/9 17:31
+ */
+
+public class gitdemo {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
